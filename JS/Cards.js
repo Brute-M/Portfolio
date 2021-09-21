d8132123cb239ea3a3da1ea79e570913cc61b70d
Brute-M <74922093+Brute-M@users.noreply.github.com>
2021-09-21 17:55:22 +0530

    Completed logic of fetching data from a file! and now refining more projects to add them here!

---
+++ JS/Cards.js
@@ -0,0 +1,53 @@
+class Cards {
+    constructor(divID /* Div ID where to append all the cards */ ) {
+        this.divID = divID
+    }
+
+
+    createCard(data) {
+        let cardElement = document.createElement("div");
+        const { title, descs, projectLink, githubLink, img, themeColor } = data
+        let cardHTML = `
+                        <div class="col my-3">
+                            <div class="card border">
+                                <div class="card_image">
+                                    <img class="img-fluid" src="${img}" alt="" srcset="">
+                                </div>
+                                <div class="openContent">
+                                    <div class="cardContent">
+                                        <h2>${title}</h2>
+                                        ${
+                                            descs.map((e)=>{
+                                                return "<p>" + e + "</p>"
+                                            })
+                                        }
+                                        <div class="cardButtons">
+                                            <button class="btn gitHubButton mx-2" onclick="window.open('${!githubLink ? "#" : githubLink}')" ${!githubLink ? "disabled='true'" : ""}>
+                                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+                                                    alt="">
+                                                <span class="mx-1">GitHub</span>
+                                                <span class="forward mx-1"><i class="fas fa-arrow-circle-right"></i></span>
+                                            </button>
+                                            <button class="btn projectButton mx-1" style="background-color: ${themeColor ? themeColor : "transparent"};" onclick="window.open('${projectLink}')" ${!projectLink ? "disabled='true'" : ""}>
+                                                <span>Project</span>
+                                                <span class="forward mx-2"><i class="fas fa-arrow-circle-right"></i></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        `
+        cardElement.innerHTML = cardHTML
+
+        return cardElement.firstElementChild
+    }
+
+
+    appendCard(card) {
+        $(`#${this.divID}`).append(card)
+    }
+}
+
+
+export default Cards
---
+++ JS/main.js
@@ -0,0 +1,12 @@
+import Cards from "./Cards.js";
+
+
+const card = new Cards("allCards" /* ID where I want to append all cards */ )
+
+$.getJSON("./JS/data.json", data => {
+    console.log(`data`, data)
+    data.forEach(e => {
+        const newCard = card.createCard(e);
+        card.appendCard(newCard)
+    })
+});
