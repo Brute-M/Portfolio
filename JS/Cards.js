dbb4894e19eecc9286a5b49d503b291d5a2acda4
Brute-M <74922093+Brute-M@users.noreply.github.com>
2021-09-21 20:11:43 +0530

    Fixed Comma issue while iteratrating descs array from data.json(got help from Vikash), also added postmanmini Project

--- JS/Cards.js
+++ JS/Cards.js
@@ -2,11 +2,22 @@ class Cards {
     constructor(divID /* Div ID where to append all the cards */ ) {
         this.divID = divID
     }
-
+    
+    getDescs(descs){
+        let d = ""
+        descs.forEach(e=>{
+            d += "<p>" + e + " </p>"
+        })
+        console.log(`d`, d)
+        return d
+    }
 
     createCard(data) {
         let cardElement = document.createElement("div");
         const { title, descs, projectLink, githubLink, img, themeColor } = data
+
+        
+
         let cardHTML = `
                         <div class="col my-3">
                             <div class="card border">
@@ -17,9 +28,10 @@ class Cards {
                                     <div class="cardContent">
                                         <h2>${title}</h2>
                                         ${
-                                            descs.map((e)=>{
-                                                return "<p>" + e + "</p>"
-                                            })
+                                            // this.getDescs(descs)
+                                            descs.map(e=>{
+                                                return "<p>" + e + " </p>"
+                                            }).join('')
                                         }
                                         <div class="cardButtons">
                                             <button class="btn gitHubButton mx-2" onclick="window.open('${!githubLink ? "#" : githubLink}')" ${!githubLink ? "disabled='true'" : ""}>
